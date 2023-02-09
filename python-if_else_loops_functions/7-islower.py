cc6fd506b1bfd23ba25911fd38efcc3992f95a18
MucyoTonny <b.mucyo@alustudent.com>
2023-02-09 06:32:53 -0800

    loops

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def uppercase(string):
-    result = ""
-    for char in string:
-        if ord(char) >= 97 and ord(char) <= 122:
-            result += chr(ord(char) - 32)
-        else:
-            result += char
-    print("{}\n".format(result), end="")
+def islower(c):
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
