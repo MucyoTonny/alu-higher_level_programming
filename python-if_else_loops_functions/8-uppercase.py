150ffa9cdb108ade831451a878ef65768b2bf7cb
MucyoTonny <b.mucyo@alustudent.com>
2023-02-09 06:40:10 -0800

    uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(string):
+    result = ""
+    for char in string:
+        if ord(char) >= 97 and ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}\n".format(result), end="")
