63d655901a9e2e5ebf034c3ae81d73755587dfda
MucyoTonny <b.mucyo@alustudent.com>
2023-02-28 02:31:07 -0800

    py module

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    add = 0
+    if length == 1:
+        add = 0
+    else:
+        for c in range(1, length):
+            add += int(sys.argv[c])
+    print(add)
