f37a97848417eb514aaff9f9fffcaecf8d45214a
MucyoTonny <b.mucyo@alustudent.com>
2023-02-28 02:28:42 -0800

    py modules

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    kk = len(sys.argv)
+    if kk == 1:
+        print("{} arguments.".format(kk - 1))
+    elif kk == 2:
+        print("{} argument:".format(kk - 1))
+    else:
+        print("{} arguments:".format(kk - 1))
+    for c in range(1, kk):
+        print("{}: {}".format(c, sys.argv[c]))
