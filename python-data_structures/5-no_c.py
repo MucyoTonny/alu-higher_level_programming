4c4062a5e9ca2132801faa65dc42de881f6cf7ca
MucyoTonny <b.mucyo@alustudent.com>
2023-03-07 00:33:42 -0800

    data structures

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    char_list = list(my_string)
+    for char in ['c', 'C']:
+        while char in char_list:
+            char_list.remove(char)
+    new_string = "".join(char_list)
+    return new_string
