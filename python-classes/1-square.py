8dc5ece7009746c790a7a528a0edbd7c76c6029b
MucyoTonny <b.mucyo@alustudent.com>
2023-03-16 01:43:22 -0700

    py classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module documented
+author is Kenny i guess we already know that
+right?"""
+
+
+class Square:
+    """ Class documented
+    """
+    def __init__(self, size):
+        self._Square__size = size
