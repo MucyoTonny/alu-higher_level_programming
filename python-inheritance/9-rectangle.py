2462d612e19ddde08ede20e74d83630dda357a3f
MucyoTonny <b.mucyo@alustudent.com>
2023-03-24 02:35:43 -0700

    py inheritance

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Student:
-    """Represent a student."""
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
-        return self.__dict__
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
