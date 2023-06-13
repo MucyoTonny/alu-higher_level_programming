6ea65ef87c53017daee5fd18be1f51a3e6c685fe
MucyoTonny <b.mucyo@alustudent.com>
2023-06-13 02:30:25 -0700

    j.script

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
-!/usr/bin/node
-
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing')
+#!/usr/bin/node
+const x = 'C is fun';
+const y = 'Python is cool';
+const z = 'JavaScript is amazing';
+console.log(x);
+console.log(y);
+console.log(z);
